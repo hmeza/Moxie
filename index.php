a42947177bb941c2a351e88eb0a35dea470491fc
Hugo Meza <hugo.meza.macias@gmail.com>
2011-02-22 01:14:34 +0000

    Included Registry and Db/Table from index. Added db var into zend registry. Removed blog and news from controllers list.

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ echo web_menu();
 include("../Zend/Zend/Controller/Front.php");
 include_once('../Zend/Zend/Db.php');
 include_once('../Zend/Zend/Db/Adapter/Pdo/Mysql.php');
+include '../Zend/Zend/Registry.php';
+include_once '../Zend/Zend/Db/Table.php';
 
 $db = new Zend_Db_Adapter_Pdo_Mysql(array(
     'host'     => '127.0.0.1',
@@ -21,15 +23,21 @@ $db = new Zend_Db_Adapter_Pdo_Mysql(array(
 ));
 $GLOBALS['db'] = $db;
 
+if ($db != null) {
+	Zend_Registry::set('db', $db);
+}
+else {
+	throw new Exception('cannot create database adapter');
+}
+Zend_Db_Table_Abstract::setDefaultAdapter($db);
+
 $front = Zend_Controller_Front::getInstance();
  
 // Set several module directories at once:
 $front->setControllerDirectory(array(
     'default' => 'application/controllers',
 	'categories'	=>	'application/controllers',
-	'expenses'		=>	'application/controllers'/*,
-    'blog'    => '../modules/blog/controllers',
-    'news'    => '../modules/news/controllers',*/
+	'expenses'		=>	'application/controllers'
 ));
 
 $front->run('application/controllers/');
