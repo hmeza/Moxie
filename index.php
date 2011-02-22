e8e9790d7b90f2efe609de9b5333fb60d4d1a46c
Hugo Meza <hugo.meza.macias@gmail.com>
2011-02-22 12:51:53 +0000

    Modified relative zend paths to fit with any configuration. Modified exception to add app name.

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ include_once 'html/web.php';
 echo web_header();
 echo web_menu();
 
-include("../Zend/Zend/Controller/Front.php");
-include_once('../Zend/Zend/Db.php');
-include_once('../Zend/Zend/Db/Adapter/Pdo/Mysql.php');
-include '../Zend/Zend/Registry.php';
-include_once '../Zend/Zend/Db/Table.php';
+include 'Zend/Controller/Front.php';
+include_once 'Zend/Db.php';
+include_once 'Zend/Db/Adapter/Pdo/Mysql.php';
+include 'Zend/Registry.php';
+include_once 'Zend/Db/Table.php';
 
 $db = new Zend_Db_Adapter_Pdo_Mysql(array(
     'host'     => '127.0.0.1',
@@ -27,7 +27,7 @@ if ($db != null) {
 	Zend_Registry::set('db', $db);
 }
 else {
-	throw new Exception('cannot create database adapter');
+	throw new Exception('Moxie: cannot create database adapter');
 }
 Zend_Db_Table_Abstract::setDefaultAdapter($db);
 
