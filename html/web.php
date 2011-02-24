820ae200fe22d449dc66b1f65715a7b17bc61752
Hugo Meza <hugo.meza.macias@gmail.com>
2011-02-24 23:06:50 +0000

    Refactoring of web_header and index

--- html/web.php
+++ html/web.php
@@ -1,9 +1,9 @@
 <?php
 // TODO: Find a proper name for this
 
-function web_header() {
+function web_header($s_name) {
 	return '
-	<h1>Welcome to <a href="http://moxie.dev/">Moxie</a></h1>
+	<h1>Welcome to <a href="http://moxie.dev/">'.$s_name.'</a></h1>
 	';
 	
 }
@@ -13,14 +13,16 @@ function web_menu() {
 	<table cellspacing=10>
 		<td><a href="/">Index</a></td>
 		<td><a href="/categories/index">Categories</a></td>
+		<td><a href="/incomes/index">Incomes</a></td>
 		<td><a href="/expenses/index">Expenses</a></td>
 	</table>
 	';
 }
 
 function web_footer() {
-	
-	
+	return '
+	<table><td>&copy; 2011 Hytsolutions.com</td></table>
+	';	
 }
 
 ?>
