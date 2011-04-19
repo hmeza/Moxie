a60fa98c4ae404c5cdb52bb01f50c7da9b63d66d
Hugo Meza <hugo.meza.macias@gmail.com>
2011-04-19 01:37:44 +0000

    First approach of budgets module.

---
+++ application/controllers/BudgetsController.php
@@ -0,0 +1,50 @@
+<?php
+/** Zend_Controller_Action */
+include 'application/models/Budgets.php';
+include 'application/models/Categories.php';
+class BudgetsController extends Zend_Controller_Action
+{
+	private $budgets;
+	
+	public function init() {
+		$this->budgets = new Budgets();		
+	}
+	
+	public function indexAction() {
+		$o_categories = new Categories();
+		$st_categories = $o_categories->getCategoriesByUser($_SESSION['user_id']);
+		$this->view->assign('categories',$st_categories);
+		//$this->_helper->redirector('index','budgets');
+	}
+	
+	/**
+	 * @desc	Add a line on budget
+	 */
+	public function addAction() {
+		$o_income = $this->getRequest()->getPost();
+		$st_data = array(
+			'id'			=>	$o_income['id'],
+			'user_owner'	=>	$_SESSION['user_id'],
+			'category'		=>	$o_income['category'],
+			'amount'		=>	$o_income['amount'],
+			'date_created'	=>	date('Y-m-d H:i:s'),
+		);
+		try {
+			$result = $this->budgets->insert($st_data);
+		}
+		catch (Exception $e) {
+			error_log("Exception caught in ".__CLASS__."::".__FUNCTION__." on line ".$e->getLine().": ".$e->getMessage());
+			try {
+				$cond = 'id = '.$st_data['id'];
+				unset($st_data[id]);
+				$result = $this->budgets->update($st_data,$cond);
+			}
+			catch(Exception $e) {
+				error_log("Exception caught in ".__CLASS__."::".__FUNCTION__." on line ".$e->getLine().": ".$e->getMessage());
+			}
+			error_log($result.' row(s) updated');
+		}
+		$this->_helper->redirector('index','budgets');
+	}
+}
+?>
