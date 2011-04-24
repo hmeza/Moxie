ae087019282b1fdb7592bc603d631de55dea487f
Hugo Meza <hugo.meza.macias@gmail.com>
2011-04-24 18:19:09 +0000

    Refactoring of Categories. Now categories can be added and deleted. New methods added to Categories model.

--- application/controllers/CategoriesController.php
+++ application/controllers/CategoriesController.php
@@ -9,23 +9,58 @@ class CategoriesController extends Zend_Controller_Action
 		$this->categories = new Categories();		
 	}
 	
-    public function indexAction()
-    {
-		$this->view->assign('list', $this->categories->getCategoriesByUser('1'));
+	private function getForm() {
+    	include('Zend/Form.php');
+    	$form  = new Zend_Form();
+    	$form->addElement('select', 'parent', array(
+			'label' => 'Category parent',
+			'multioptions' => $this->categories->getCategoriesForView()
+			)
+		);
+		$form->addElement('text', 'name', array('label' => 'Category name'));
+		$form->addElement('text', 'description', array('label' => 'Category description'));
+		$form->addElement('submit','submit', array('label' => 'Enviar'));
+    	
+		return $form;
+	}
+	
+	private function mountCategoryTree($st_categories) {
+		$st_orderedCategories = array();
+		return $st_categories;
+	}
+		
+    public function indexAction() {
+		$this->view->assign('form', $this->getForm());
+		$this->view->assign('list', $this->mountCategoryTree($this->categories->getCategoriesByUser($_SESSION['user_id'])));
     }
     
    
     public function addAction() {
     	try {
 	    	$data = $this->_request->getParams();
-	    	
 	    	$this->categories->addCategory($data);
-	    	
-	    	$this->view->assign('list', $this->categories->getCategoriesByUser('1'));
-	    	$this->render('index');
-    	} catch (Exception $e) {
-    		echo 'Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage();
-    	}
+		} catch (Exception $e) {
+    		error_log('Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage());
+    	}	    	
+    	$this->view->assign('form', $this->getForm());
+    	$this->view->assign('list', $this->mountCategoryTree($this->categories->getCategoriesByUser($_SESSION['user_id'])));
+    	$this->render('index');
+    }
+    
+    public function deleteAction() {
+    	// check if category has expenses or incomes
+    	// if so, assign it before deleting
+    	// delete category
+		$i_id = $this->getRequest()->getParam('id');
+		try {
+			$this->categories->delete('id = '.$i_id);
+		}
+		catch (Expenses $e) {
+			error_log('Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage());
+		}
+		$this->view->assign('form', $this->getForm());
+    	$this->view->assign('list', $this->mountCategoryTree($this->categories->getCategoriesByUser($_SESSION['user_id'])));
+    	$this->render('index');
     }
 }
 ?>
