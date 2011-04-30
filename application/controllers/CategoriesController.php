c6ee54e306dca83b2e8ad4b7a622a2e77600f100
Hugo Meza <hugo.meza.macias@gmail.com>
2011-04-30 03:30:51 +0000

    Implemented edit category. Refactoring.

--- application/controllers/CategoriesController.php
+++ application/controllers/CategoriesController.php
@@ -12,15 +12,40 @@ class CategoriesController extends Zend_Controller_Action
 	private function getForm() {
     	include('Zend/Form.php');
     	$form  = new Zend_Form();
+    	
+    	$form->setAction('/categories/add')->setMethod('post');
     	$form->addElement('select', 'parent', array(
 			'label' => 'Category parent',
-			'multioptions' => $this->categories->getCategoriesForSelect()
+			'multioptions' => $this->categories->getCategoriesForSelect(),
 			)
 		);
 		$form->addElement('text', 'name', array('label' => 'Category name'));
 		$form->addElement('text', 'description', array('label' => 'Category description'));
 		$form->addElement('submit','submit', array('label' => 'Enviar'));
     	
+		return $form;
+	}
+	
+	private function getEditForm($i_categoryPK) {
+		include('Zend/Form.php');
+		$form  = new Zend_Form();
+		
+		// retrieve data to fill the form
+		$st_category = $this->categories->find($i_categoryPK);
+
+		$form->setAction('/categories/update')->setMethod('post');
+		
+		$form->addElement('hidden', 'id', array('value' => $i_categoryPK));
+		// Add select
+		$form->addElement('select', 'parent', array(
+			'label' => 'Category parent',
+			'multioptions' => $this->categories->getCategoriesForSelect(),
+			'value'	=>	$st_category[0]['parent']
+			)
+		);
+		$form->addElement('text', 'name', array('label' => 'Category name', 'value' => $st_category[0]['name']));
+		$form->addElement('text', 'description', array('label' => 'Category description', 'value' => $st_category[0]['description']));
+		$form->addElement('submit','submit', array('label' => 'Enviar'));
 		return $form;
 	}
 	
@@ -39,7 +64,6 @@ class CategoriesController extends Zend_Controller_Action
 		foreach ($st_categories as $key => $value) {
 			$st_parentCategories[] = $value;
 		}
-		error_log(print_r($st_parentCategories,true));
 		return $st_parentCategories;
 	}
 		
@@ -56,13 +80,32 @@ class CategoriesController extends Zend_Controller_Action
 		} catch (Exception $e) {
     		error_log('Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage());
     	}	    	
-    	$this->view->assign('form', $this->getForm());
+    	$this->_helper->redirector('index','categories');
+    }
+    
+    public function editAction() {
+    	$this->view->assign('form', $this->getEditForm($this->_request->getParam('id')));
     	$this->view->assign('list', $this->mountCategoryTree($this->categories->getCategoriesByUser($_SESSION['user_id'])));
     	$this->render('index');
     }
     
+    public function updateAction() {
+        try {
+	    	$data = $this->_request->getParams();
+	    	$st_update = array(
+	    		'name'	=>	$data['name'],
+	    		'description'	=>	$data['description'],
+	    		'parent'		=>	$data['parent']
+	    	);
+	    	$this->categories->update($st_update,'id = '.$data['id'].' AND user_owner = '.$_SESSION['user_id']);
+		} catch (Exception $e) {
+    		error_log('Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage());
+    	}
+    	$this->_helper->redirector('index','categories');
+    }
+    
     public function deleteAction() {
-    	// check if category has expenses or incomes
+    	// TODO: check if category has expenses or incomes
     	// if so, assign it before deleting
     	// delete category
 		$i_id = $this->getRequest()->getParam('id');
@@ -72,9 +115,7 @@ class CategoriesController extends Zend_Controller_Action
 		catch (Expenses $e) {
 			error_log('Exception caught on '.__CLASS__.', '.__FUNCTION__.'('.$e->getLine().'), message: '.$e->getMessage());
 		}
-		$this->view->assign('form', $this->getForm());
-    	$this->view->assign('list', $this->mountCategoryTree($this->categories->getCategoriesByUser($_SESSION['user_id'])));
-    	$this->render('index');
+		$this->_helper->redirector('index','categories');
     }
 }
 ?>
