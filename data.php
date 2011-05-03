b9e9787ad31eaf2d012e02f1186bd65892b11711
Hugo Meza <hugo.meza.macias@gmail.com>
2011-05-03 02:15:28 +0000

    Fixed root category bug in data graph from expenses.

--- data.php
+++ data.php
@@ -3,7 +3,6 @@ $data = explode(":",$_GET['mydata']);
 $month = (empty($data[1])) ? date('n') : $data[1];
 $year = (empty($data[3])) ? date('Y') : $data[3];
 $user = (empty($data[5])) ? 0 : $data[5]; 
-
 // use the chart class to build the chart:
 include_once( 'application/3rdparty/ofc/open-flash-chart.php' );
 $g = new graph();
@@ -17,6 +16,13 @@ $g->pie(60,'#000000','{font-size: 12px; color: #000000;');
 // get categories from DB
 mysql_connect("127.0.0.1","root","0nr3fn1");
 mysql_select_db("moxie");
+
+// get root category ID
+$sql = "select id from categories where parent IS NULL and user_owner = ".$user;
+$data = mysql_query($sql);
+$s_id = mysql_fetch_array($data);
+$i_id = $s_id['id'];
+
 $sql = "select c0.id as parent_id, c.name as name,sum(e.amount) from expenses e, categories c left join categories c2 on c.id = c2.parent left join categories c0 on c0.id = c.parent "
 	." where (c.id = e.category or c2.id = e.category) AND YEAR(e.expense_date) = ".$year." AND MONTH(e.expense_date) = ".$month
 	." and e.in_sum = 1 and e.user_owner = ".$user
@@ -26,7 +32,7 @@ $rows = mysql_query($sql);
 $data = array();
 $labels = array();
 while ($value = mysql_fetch_array($rows)) {
-	if ($value['parent_id'] == 1) {
+	if ($value['parent_id'] == $i_id) {
 		$data[] = $value[2];
 		$labels[] = $value[1];
 	}
