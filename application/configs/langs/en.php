2848d2891b77c3745b40b6df5f790d050abcb55a
Hugo Meza <hugo.meza.macias@gmail.com>
2011-05-03 23:50:06 +0000

    Stats adapted to english.

--- application/configs/langs/en.php
+++ application/configs/langs/en.php
@@ -1,6 +1,7 @@
 <?php
 // English
 $st_lang = array(
+	// Index and menu
 	'welcome' => 'Welcome to ',
 	'categories' => 'Categories',
 	'budget'	=>	'Budget',
@@ -11,5 +12,15 @@ $st_lang = array(
 	'about'		=>	'About',
 	'benefits'	=>	'Benefits',
 	'new_user'	=>	'Already don\'t have an account?',
+	// Expenses
+	// Incomes
+	// Budgets
+	// Categories
+	// Stats
+	'category'	=>	'Category',
+	'total_this_year'	=>	'Total this year',
+	'average_this_year'	=>	'Average this year',
+	'total'		=>	'Total',
+	'average'	=>	'Average'
 );
 ?>
