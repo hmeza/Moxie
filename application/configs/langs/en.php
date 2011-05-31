78223ad3af6090aee88b8898b5fe4ff5491b28ab
Hugo Meza <hugo.meza.macias@gmail.com>
2011-05-31 22:21:22 +0000

    Added logout translation in english and spanish.

--- application/configs/langs/en.php
+++ application/configs/langs/en.php
@@ -36,6 +36,7 @@ $st_lang = array(
 	'register'	=>	'Registration form',
 	'registration_ok'	=>	'<h2>Registration successfully!</h2>
 Now you can log in with your login and password to start using Moxie.',
+	'logout'	=>	'Logout',
 	// Other stuff
 );
 ?>
