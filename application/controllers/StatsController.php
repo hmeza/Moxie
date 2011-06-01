43bc5656ce0fe2220361dd6ed94b4a86e26c240f
Hugo Meza <hugo.meza.macias@gmail.com>
2011-06-01 00:11:55 +0000

    StatsController refactoring.

--- application/controllers/StatsController.php
+++ application/controllers/StatsController.php
@@ -4,6 +4,7 @@ include_once( 'application/models/Expenses.php');
 include_once( 'application/models/Incomes.php');
 include_once( 'application/models/Categories.php');
 include_once '../Zend/Zend/Db/Expr.php';
+include_once( 'application/3rdparty/ofc/open-flash-chart.php' );
 
 class StatsController extends Zend_Controller_Action {
 	private $expenses;
@@ -16,52 +17,14 @@ class StatsController extends Zend_Controller_Action {
 		$this->categories = new Categories();
 	}
 	
-	public function incomeStats() {
-	
-	}
-	
 	/**
-	 * @desc	Print detailed stats from user expenses and incomes.
-	 * @todo	Use a group by to retrieve data and match with array
-	 */
-	public function indexAction() {
-		$incomeStatsByCategory = $this->categories->getCategoriesForView();
-		$data = array();
-		$db = Zend_Registry::get('db');
-		foreach ($incomeStatsByCategory as $key => $value) {
-			$data[$key]['index'] = $key;
-			$data[$key]['name'] = $value;
-			$s_select = $db->select()
-				->from('expenses',
-					array(
-						new Zend_Db_Expr('SUM(amount)')
-					)
-				)
-				->where("user_owner = ".$_SESSION['user_id'])
-				->where("category = ".$key);
-			$st_data = $db->fetchRow($s_select);
-			$data[$key]['sumtotal'] = $st_data['SUM(amount)'];
-			$s_select = $db->select()
-				->from('expenses',
-					array(
-						new Zend_Db_Expr('SUM(amount)'),
-						new Zend_Db_Expr('AVG(amount)')
-					)
-				)
-				->where("user_owner = ".$_SESSION['user_id'])
-				->where("category = ".$key)
-				->where("YEAR(expense_date) = ".date('Y'));
-			$st_data = $db->fetchRow($s_select);
-			$data[$key]['sumyear'] = $st_data['SUM(amount)'];
-			$data[$key]['avgyear'] = $st_data['AVG(amount)'];
-		}
-		$this->view->assign('data', $data);
-	}
-	
+	 * @desc	Generates year income graph.
+	 * @author	hmeza
+	 * @since	2011-06-01
+	 */	
 	public function incomestatsAction() {
 		$mydata = $this->getRequest()->getParam('mydata');
 		// use the chart class to build the chart:
-		include_once( 'application/3rdparty/ofc/open-flash-chart.php' );
 		$data = explode(":",$mydata);
 		$user = (empty($data[1])) ? 0 : $data[1];
 		
@@ -118,85 +81,11 @@ class StatsController extends Zend_Controller_Action {
 		$this->_helper->viewRenderer->setNoRender();
 	}
 	
-	// TODO: generate graphs
-	public function statsAction() {
-		$mydata = $this->getRequest()->getParam('mydata');
-		// use the chart class to build the chart:
-		include_once( 'application/3rdparty/ofc/open-flash-chart.php' );
-		
-		$data = explode(":",$mydata);
-		//$category = (empty($data[1])) ? 0 : $data[1];
-		$scale = (empty($category)) ? 2500 : 500;
-		$user = (empty($data[1])) ? 0 : $data[1];
-		
-		mysql_connect("127.0.0.1","root","0nr3fn1");
-		mysql_select_db("moxie");
-		$sql = 'select YEAR(expense_date), MONTH(expense_date),sum(amount)'
-		.' from expenses'
-		.' where in_sum = 1 AND user_owner = '.$user;
-		if ($category != 0) $sql .= ' AND category = '.$category;
-		$sql .= ' group by MONTH(expense_date),YEAR(expense_date)'
-		.' order by YEAR(expense_date),MONTH(expense_date)';
-		$rows = mysql_query($sql);
-		
-		$data = array();
-		$labels = array();
-		
-		$bar = new bar_outline(50, '#060606', '#040404');
-		$bar->key('By months', 10);
-		$bar->data = array();
-		$months = array();
-		while ($value = mysql_fetch_array($rows)) {
-			$bar->data[] = $value[2];
-			$timestamp = mktime(0, 0, 0, $value[1], 1, 2005);
-			$months[] = date("M", $timestamp);
-		}
-		mysql_free_result($rows);
-		
-		$g = new graph();
-		$g->title( 'Monthly expense', '{font-size: 20px;}' );
-		$timestamp = mktime(0, 0, 0, $month, 1, $year);
-		$s_month = date("F", $timestamp);
-		$g->data_sets[] = $bar;
-		$g->bg_colour = '0xEFFFEF';	// soft green
-		$g->bg_colour = '0xE3F0FD';
-		$g->bg_colour = '0xFFFFFF';
-		$g->set_x_labels( $months );
-		//
-		// set the X axis to show every 2nd label:
-		//
-		$g->set_x_label_style( 10, '#9933CC', 0, 1 );
-		//
-		// and tick every second value:
-		//
-		$g->set_x_axis_steps( 1 );
-		//
-		$g->set_inner_background( '#E3F0FD', '#ABD7E6', 90 );
-		
-		$g->set_y_max( $scale );
-		$g->y_label_steps( 4 );
-		$g->set_y_legend( 'Monthly expenses', 12, '#736AFF' );
-		echo $g->render();
-		
-		// display the data
-		echo $g->render();
-		$this->_helper->viewRenderer->setNoRender();
-	}
-	
-	// TODO: generate graphs 2
-	public function generate_graphs() {
-		// Sacar suma de amounts ordenada por mes y año
-		$sql = 'select YEAR(expense_date), MONTH(expense_date),sum(amount)'
-		.' from expenses group by MONTH(expense_date),YEAR(expense_date)'
-		.' order by YEAR(expense_date),MONTH(expense_date)';
-		
-		// Sacar suma de amounts ordenada por mes y año por categoria
-		$sql = 'select YEAR(expense_date), MONTH(expense_date),sum(amount)'
-		.' from expenses group by MONTH(expense_date),YEAR(expense_date)'
-		.' where category = ?'
-		.' order by YEAR(expense_date),MONTH(expense_date)';
-	}
-	
+	/**
+	 * @desc	Generates expenses pie graph.
+	 * @author	hmeza
+	 * @since	2011-06-01
+	 */
 	public function dataAction() {
 		$mydata = $this->getRequest()->getParam('mydata');
 		$data = explode(":",$mydata);
@@ -205,7 +94,7 @@ class StatsController extends Zend_Controller_Action {
 		$year = (empty($data[3])) ? date('Y') : $data[3];
 		$user = (empty($data[5])) ? 0 : $data[5]; 
 		// use the chart class to build the chart:
-		include_once( 'application/3rdparty/ofc/open-flash-chart.php' );
+
 		$g = new graph();
 		
 		$g->bg_colour = '0xFFFFFF';
@@ -250,6 +139,73 @@ class StatsController extends Zend_Controller_Action {
 		$s_month = date("F", $timestamp);
 		$g->title( $s_month." ".$year, '{font-size:18px; color: #000000}' );
 		
+		// display the data
+		echo $g->render();
+		$this->_helper->viewRenderer->setNoRender();
+	}
+	
+	/**
+	 * @desc	Generates monthly expenses graph.
+	 * @author	hmeza
+	 * @since	2011-06-01
+	 */
+	public function statsAction() {
+		global $st_lang;
+		$mydata = $this->getRequest()->getParam('mydata');
+		// use the chart class to build the chart:
+		
+		$data = explode(":",$mydata);
+		//$category = (empty($data[1])) ? 0 : $data[1];
+		$scale = (empty($category)) ? 2500 : 500;
+		$user = (empty($data[1])) ? 0 : $data[1];
+		
+		mysql_connect("127.0.0.1","root","0nr3fn1");
+		mysql_select_db("moxie");
+		$sql = 'select YEAR(expense_date), MONTH(expense_date),sum(amount)'
+		.' from expenses'
+		.' where in_sum = 1 AND user_owner = '.$user;
+		if ($category != 0) $sql .= ' AND category = '.$category;
+		$sql .= ' group by MONTH(expense_date),YEAR(expense_date)'
+		.' order by YEAR(expense_date),MONTH(expense_date)';
+		$rows = mysql_query($sql);
+		
+		$data = array();
+		$labels = array();
+		
+		$bar = new bar_outline(50, '#060606', '#040404');
+		$bar->key($st_lang['expenses_by_months'], 10);
+		$bar->data = array();
+		$months = array();
+		while ($value = mysql_fetch_array($rows)) {
+			$bar->data[] = $value[2];
+			$timestamp = mktime(0, 0, 0, $value[1], 1, 2005);
+			$months[] = date("M", $timestamp);
+		}
+		mysql_free_result($rows);
+		
+		$g = new graph();
+		$g->title( $st_lang['expenses_monthly'] , '{font-size: 20px;}' );
+		$timestamp = mktime(0, 0, 0, $month, 1, $year);
+		$s_month = date("F", $timestamp);
+		$g->data_sets[] = $bar;
+		$g->bg_colour = '0xEFFFEF';	// soft green
+		$g->bg_colour = '0xE3F0FD';
+		$g->bg_colour = '0xFFFFFF';
+		$g->set_x_labels( $months );
+		//
+		// set the X axis to show every 2nd label:
+		//
+		$g->set_x_label_style( 10, '#9933CC', 0, 1 );
+		//
+		// and tick every second value:
+		//
+		$g->set_x_axis_steps( 1 );
+		//
+		$g->set_inner_background( '#E3F0FD', '#ABD7E6', 90 );
+		
+		$g->set_y_max( $scale );
+		$g->y_label_steps( 4 );
+		$g->set_y_legend( $st_lang['expenses_monthly'], 12, '#736AFF' );
 		// display the data
 		echo $g->render();
 		$this->_helper->viewRenderer->setNoRender();
