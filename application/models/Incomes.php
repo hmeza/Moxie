2b4ab95fb22288a5b64941b4ab3ae606e6bbb312
Hugo Meza <hugo.meza.macias@gmail.com>
2011-06-13 22:48:50 +0000

    Refactoring of Incomes.

--- application/models/Incomes.php
+++ application/models/Incomes.php
@@ -10,24 +10,5 @@ class Incomes extends Zend_Db_Table_Abstract {
 	public function __construct() {
 		$this->_db = Zend_Registry::get('db');
 	}
-	
-	public function getIncomesList($i_year) {
-		$st_data = array(
-			'sum'	=>	'sum(i.amount)',
-			'name'	=>	'c.name'
-		);
-		try {
-			$i_month = 1;
-			$s_select = $this->_db->select()
-				->from(array('i'=>'incomes'), $st_data)
-				->joinLeft(array('c'=>'categories','c.id = i.category',array()))
-				->where('YEAR(i.date) = '.$i_year)
-				->where('MONTH(i.date) = '.$i_month)
-				->where('user_owner = '.$_SESSION['user_id']);
-		} catch (Exception $e) {
-			error_log("Exception caught in ".__CLASS__."::".__FUNCTION__." on line ".$e->getLine().": ".$e->getMessage());
-		}
-	}
 }
-
 ?>
