c9708b50ee20eee167494e5fa6e68494b6d6a3e7
Hugo Meza <hugo.meza.macias@gmail.com>
2011-11-07 04:51:07 +0000

    Refactoring: Moved application bootstrap to Zend Bootstrap class.

---
+++ application/Bootstrap.php
@@ -0,0 +1,109 @@
+<?php
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+	
+	protected function _initLibraries() {
+		//Zend Loader
+		require_once "Zend/Loader.php";  
+  
+		//Required classes 
+		Zend_Loader::loadClass('Zend_Controller_Front');
+		Zend_Loader::loadClass('Zend_Registry');  
+		//Zend_Loader::loadClass('Zend_Session');
+		//Zend_Loader::loadClass('Zend_Auth');  
+		Zend_Loader::loadClass('Zend_Db');  
+		Zend_Loader::loadClass('Zend_Db_Table');
+		Zend_Loader::loadClass('Zend_Db_Adapter_Pdo_Mysql');  
+		Zend_Loader::loadClass('Zend_Config_Ini');
+		
+		//include_once('Zend/Form.php');
+		//include_once('application/models/Categories.php');
+		//Zend_Loader::loadClass('Zend_Form');
+	}
+	
+	/**
+	 * 
+	 * Get browser platform and load library
+	 */
+	protected function _initBrowser() {
+		$navigator = get_browser(null, true);
+		error_log(print_r($navigator,true));
+		if ($navigator['platform'] == 'Android' || $navigator['browser'] == 'Android') {
+			include_once 'html/mobile.php';
+			$s_viewPrefix = "m-";
+		}
+		else {
+			include_once 'html/web.php';
+			$s_viewPrefix = "";
+		}
+	}
+	
+	/**
+	 * 
+	 * Init translation array
+	 */
+	protected function _initTranslation() {
+		// Set translation
+		if (isset($_SESSION['user_lang'])) {
+			include 'application/configs/langs/'.$_SESSION['user_lang'].'.php';
+		}
+		else {
+			include 'application/configs/langs/es.php';
+		}
+		$GLOBALS['st_lang'] = $st_lang;
+	}
+	
+	/**
+	 *
+	 * Init database
+	 * @throws Exception
+	 */
+	protected function _initDb() {
+		// Set config vars
+		switch($_SERVER['SERVER_NAME']) {
+			case 'hugoboss666.no-ip.com':
+			case 'moxie.com':
+			case 'moxie.redirectme.net':
+				$section = "staging";
+				break;
+			case 'hytsolutions.com':
+			case 'moxie.hytsolutions.com':
+				$section = "production";
+				break;
+			case 'moxie.dev':
+			default:
+				$section = "development";
+				break;
+		}
+		
+		$o_config = new Zend_Config_Ini('application/configs/application.ini', $section, array('allowModifications'=>true));
+		$o_registry = Zend_Registry::getInstance();
+		$o_registry->set('config', $o_config);
+		
+		$db = new Zend_Db_Adapter_Pdo_Mysql(array(
+		    'host'     => Zend_Registry::get('config')->moxie->db->host,
+		    'username' => Zend_Registry::get('config')->moxie->db->username,
+		    'password' => Zend_Registry::get('config')->moxie->db->password,
+		    'dbname'   => Zend_Registry::get('config')->moxie->db->database
+		));
+		$GLOBALS['db'] = $db;
+		
+		if ($db != null) {
+			Zend_Registry::set('db', $db);
+		}
+		else {
+			throw new Exception('Moxie: cannot create database adapter');
+		}
+		Zend_Db_Table_Abstract::setDefaultAdapter($db);
+	}
+
+	/**
+	 * 
+	 * Start Moxie
+	 */
+	protected function _initApp() {
+		echo web_header(Zend_Registry::get('config')->moxie->app->name,
+				Zend_Registry::get('config')->moxie->settings->url);
+		echo web_menu();
+	}
+}
