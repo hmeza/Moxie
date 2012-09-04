5c3d9602e7c554117a1cb03299c1582c60cee05c
Hugo Meza <hugo.meza.macias@gmail.com>
2012-09-04 12:52:56 +0000

    Removed Google analytics part from index to prepare repo for github sharing.

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <html>
 <head>
-<meta name="google-site-verification" content="iE-pGhNJXHaAv1EEwkd5eCMStzQBHUtGZaiwKN-WJoA" />
 <link rel="stylesheet" type="text/css" href="moxie.css"/>
 <link rel="stylesheet" type="text/css" href="dropdown.css"/>
-<!-- Google Analytics script -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-10754954-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </head>
 <body>
 <div style="min-height: 100%; height: auto !important; height: 100%; margin: 0 auto -4em;">
