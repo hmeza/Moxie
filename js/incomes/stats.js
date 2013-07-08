95402bef73595e78c88a845317328ce10b420511
Hugo Meza <hugo.meza.macias@gmail.com>
2013-07-09 01:14:20 +0200

    Added incomes graph generated with google charts. Refs #14.

---
+++ js/incomes/stats.js
@@ -0,0 +1,17 @@
+var chart;
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+// Callback function
+function drawChart() {
+    var data = google.visualization.arrayToDataTable(barChartData);
+    
+	var options = {
+		title: barTitle,
+		hAxis: {title: barYearLabel, titleTextStyle: {color: 'green'}}
+	};
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('incomes_all'));
+	chart.draw(data, options);
+}
