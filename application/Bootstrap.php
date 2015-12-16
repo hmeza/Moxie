da66b0d784ca93f4729a87016cbb91e868a9b1dc
Hugo Meza <hugo.meza.macias@gmail.com>
2015-12-16 11:49:08 +0100

    Removed mobile detection.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -80,32 +80,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	}
 	
 	/**
-	 * Detect type of browser
-	 */
-	protected function _initBrowserDetection() {
-		// Detect mobile
-		if(!isset($_SESSION['device'])) {
-			include 'application/3rdparty/mobile-detect/Mobile_Detect.php';
-			$detect = new Mobile_Detect();
-		
-			if($detect->isMobile() && !$detect->isTablet())
-				$_SESSION['device'] = 'mobile';
-			else if($detect->isTablet())
-				$_SESSION['device'] = 'tablet';
-			else
-				$_SESSION['device'] = 'desktop';
-		}
-		else {
-			if ($_SESSION['device'] == "mobile") {
-			}
-			if($_SESSION['device'] == "tablet") {
-			}
-		}
-		// Remove this after developing
-		//$_SESSION['device'] = 'mobile';
-	}
-
-	/**
 	 * Start Moxie
 	 */
 	protected function _initApp() {
