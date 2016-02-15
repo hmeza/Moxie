8d65b9f91b1d21ab3dbbeb9e5f5fef8459fc8785
hmeza <hugo.meza.macias@gmail.com>
2016-02-15 01:54:18 +0100

    Added autosuggest for tags. Fixes in tags styles.

--- js/expenses/data.js
+++ js/expenses/data.js
@@ -19,10 +19,27 @@ var filter_tag = function() {
 
 $(document).ready(function() {
     enableSelectBoxes();
-        new Taggle('tags', {
-                tags: tagList,
-                duplicateTagClass: 'bounce'
-        });
+
+	var taggle = new Taggle('tags', {
+			tags: tagList,
+			duplicateTagClass: 'bounce'
+	});
+
+	var container = taggle.getContainer();
+	var input = taggle.getInput();
+
+	$(input).autocomplete({
+		source: usedTagList,
+		appendTo: container,
+		position: { at: "left bottom", of: container },
+		select: function(event, data) {
+			event.preventDefault();
+			//Add the tag if user clicks
+			if (event.which === 1) {
+				taggle.add(data.item.value);
+			}
+		}
+	});
 
     // selecct category and tag, if any
     $('#category_filter').val(category);
