c421c4f9589a2485cf8436a1aac97fc4afdd3811
Hugo Meza <hugo.meza.macias@gmail.com>
2016-07-30 21:05:26 +0200

    Unifying header

--- application/views/helpers/Web.php
+++ application/views/helpers/Web.php
@@ -5,4 +5,18 @@ class Moxie_View_Helper_Web extends Zend_View_Helper_Abstract {
 		global $st_lang;
 		echo '<div class="back_to_top"><br><a href="#top">'.$st_lang['back_to_top'].'</a></div>';
 	}
+
+	public function printHeaderNextLastInterval($last_url, $next_url, $current_month_and_year) {
+?>
+		<div class="moxie_time_frame_selector">
+			<a href="<?php echo $last_url; ?>">
+				<div class="moxieprevious"> < </div>
+			</a>
+			<div class="moxie_header_date"><span style="display: inline-block; line-height: normal;"><?php echo $current_month_and_year; ?></span></div>
+		<a href="<?php echo $next_url; ?>">
+			<div class="moxienext"> > </div>
+		</a>
+		</div>
+<?php
+	}
 }
