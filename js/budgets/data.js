00ad705d278c7d65229abd9d995d5fbbaaeb7ae6
Hugo Meza <hugo.meza.macias@gmail.com>
2016-08-05 19:39:20 +0200

    Refactor, moved dynamically generated js to static file

--- js/budgets/data.js
+++ js/budgets/data.js
@@ -1,10 +1,28 @@
-function getSum() {
+$(document).ready(function() {
+    $(document).ajaxStart(function () {
+        $("#spinner").show();
+    })
+    .ajaxStop(function () {
+        $("#spinner").hide();
+    });
+});
+
+function store(trigger_id) {
+    category = $("#category" + trigger_id).val();
+    amount = $("#amount" + trigger_id);
+    $.ajax({
+        url: moxie_url+"/budgets/add/category/" + category + "/amount/" + amount.val()
+    });
+}
+
+function getSum(trigger_id) {
     var sum = 0;
     $('[id^=amount]').each(function() {
         sum += parseFloat(this.value);
     });
     sum = Math.round(sum * 100) / 100;
     document.getElementById('sum').innerHTML = '<b>' + sum + ' &euro;</b>';
+    store(trigger_id);
 }
 
 function snapshot() {
