8aebe35ab0b7bd46ec70d24cd3caf4b5c102384c
Hugo Meza <hugo.meza.macias@gmail.com>
2016-09-06 23:36:04 +0200

    Code style

--- application/controllers/ExpensesController.php
+++ application/controllers/ExpensesController.php
@@ -17,81 +17,6 @@ class ExpensesController extends TransactionsController
 	}
 
 	/**
-	 * This function generates the form to add expenses.
-     * @param array $st_expense
-     * @return Zend_Form
-	 */
-	private function getForm($st_expense) {
-		global $st_lang;
-		$form  = new Zend_Form();
-
-		if(empty($st_expense['id'])) {
-			$in_sum_value = 1;
-			$slug = '/expenses/add';
-
-		}
-		else {
-			$in_sum_value = $st_expense['in_sum'];
-			$slug = '/expenses/update';
-		}
-
-        $form->setAction(Zend_Registry::get('config')->moxie->settings->url.$slug)
-            ->setMethod('post');
-		     
-		$form->setAttrib('id', 'login');
-
-		$st_categories = $this->categories->getCategoriesForView(Categories::EXPENSES);
-		asort($st_categories);
-        if(empty($st_expense['category'])) {
-            reset($st_categories);
-            $st_expense['category'] = key($st_categories);
-        }
-
-		$f_expense = !empty($st_expense['amount']) && $st_expense['amount'] != '0.00' ? $st_expense['amount'] : '';
-		$form->addElement('text', 'amount', array('label' => $st_lang['expenses_amount'], 'value' => $f_expense, 'placeholder' => '0,00'));
-        $multiOptions = new Zend_Form_Element_Select('category');
-        $multiOptions->setName('category');
-        $multiOptions->setLabel($st_lang['expenses_category']);
-        $multiOptions->addMultiOptions($st_categories);
-        $multiOptions->setValue(array($st_expense['category']));
-        $form->addElement($multiOptions);
-
-		$form->addElement("checkbox", 'in_sum', array('label' => $st_lang['in_sum_message'], 'value' => $in_sum_value, 'style' => 'width: 20px;'));
-		$form->addElement('text', 'note', array('label' => $st_lang['expenses_note'], 'value' => $st_expense['note']));
-		$form->addElement('date', 'date', array('label' => $st_lang['expenses_date'], 'value' => $st_expense['date']));
-		$form->addElement('submit','submit', array('label' => $st_lang['expenses_send']));
-        $form->addElement('hidden', 'id', array('label' => null, 'value' => $st_expense['id']));
-		return $form;
-	}
-	
-	/**
-	 * Returns the monthly expense for a year.
-	 * @return array
-	 */
-	private function getMonthExpensesData() {
-		$st_data = array();
-		$i_dateLimit = date("Y-m-01 00:00:00", strtotime("-12 months"));
-		
-		$o_rows = $this->expenses->getMonthExpensesData($_SESSION['user_id'], $i_dateLimit);
-
-		foreach ($o_rows as $key => $value) {
-			try {
-				$timestamp = mktime(0, 0, 0, $value['month'], 1, $value['year']);
-				$st_data[] = array(
-						date("M", $timestamp),
-						(float)$value['amount']
-				);
-			}
-			catch(Exception $e) {
-				error_log(__METHOD__.": ".$e->getMessage());
-				error_log(__METHOD__." data: ".$key." ".print_r($value,true));
-			}
-		}
-		$st_data = array_merge(array(array('Month', 'Expense')), $st_data);
-		return $st_data;
-	}
-
-	/**
 	 * Shows the expenses view.
 	 * Receives call from export to excel too.
 	 */
@@ -117,51 +42,6 @@ class ExpensesController extends TransactionsController
 		$this->assignViewData($st_list, $st_params, $st_expense);
 	}
 
-	/**
-	 * Exports to excel the data currently shown in the view.
-	 * @param array|array[] $st_data containing rows with columns date, amount, name and note.
-	 */
-	private function exportToExcel($st_data) {
-		$this->getResponse()
-				->setHttpResponseCode(200)
-				->setHeader('Content-Type', 'text/csv; charset=utf-8')
-				->setHeader('Content-Disposition', 'attachment; filename='.date('Y-m-d').'.csv')
-				->setHeader('Pragma', 'no-cache')
-				->sendHeaders();
-
-		$output = fopen('php://output', 'w');
-
-		fputcsv($output, array('Fecha', 'Euros', 'Categoria', 'Nota'));
-
-		foreach($st_data as $row) {
-			$outputRow = array(
-				'Fecha' => $row['date'],
-				'Euros' => $row['amount'],
-				'Categoria' => $row['name'],
-				'Nota' => $row['note']
-			);
-			fputcsv($output, $outputRow);
-		}
-		exit(0);
-	}
-
-	/**
-	 * @param array $tags
-	 * @param int $expenseId
-	 * @throws Exception
-	 */
-	private function updateTags($tags, $expenseId) {
-		$existingTags = $this->tags->getTagsByUser($_SESSION['user_id']);
-		foreach($tags as $tag) {
-            $searchTag = str_replace("'", "\\'", $tag);
-			$tagId = array_search($searchTag, $existingTags);
-			if($tagId === FALSE) {
-				$tagId = $this->tags->addTag($_SESSION['user_id'], $tag);
-			}
-			$this->transactionTags->addTagToTransaction($expenseId, $tagId);
-		}
-	}
-	
 	/**
 	 * Adds an expense and shows expenses index again
 	 * @author	hmeza
@@ -250,6 +130,126 @@ class ExpensesController extends TransactionsController
 			$this->transactionTags->getAdapter()->rollBack();
 		}
 		$this->_helper->redirector('index','expenses');
+	}
+
+	/**
+	 * This function generates the form to add expenses.
+	 * @param array $st_expense
+	 * @return Zend_Form
+	 */
+	private function getForm($st_expense) {
+		global $st_lang;
+		$form  = new Zend_Form();
+
+		if(empty($st_expense['id'])) {
+			$in_sum_value = 1;
+			$slug = '/expenses/add';
+
+		}
+		else {
+			$in_sum_value = $st_expense['in_sum'];
+			$slug = '/expenses/update';
+		}
+
+		$form->setAction(Zend_Registry::get('config')->moxie->settings->url.$slug)
+				->setMethod('post');
+
+		$form->setAttrib('id', 'login');
+
+		$st_categories = $this->categories->getCategoriesForView(Categories::EXPENSES);
+		asort($st_categories);
+		if(empty($st_expense['category'])) {
+			reset($st_categories);
+			$st_expense['category'] = key($st_categories);
+		}
+
+		$f_expense = !empty($st_expense['amount']) && $st_expense['amount'] != '0.00' ? $st_expense['amount'] : '';
+		$form->addElement('text', 'amount', array('label' => $st_lang['expenses_amount'], 'value' => $f_expense, 'placeholder' => '0,00'));
+		$multiOptions = new Zend_Form_Element_Select('category');
+		$multiOptions->setName('category');
+		$multiOptions->setLabel($st_lang['expenses_category']);
+		$multiOptions->addMultiOptions($st_categories);
+		$multiOptions->setValue(array($st_expense['category']));
+		$form->addElement($multiOptions);
+
+		$form->addElement("checkbox", 'in_sum', array('label' => $st_lang['in_sum_message'], 'value' => $in_sum_value, 'style' => 'width: 20px;'));
+		$form->addElement('text', 'note', array('label' => $st_lang['expenses_note'], 'value' => $st_expense['note']));
+		$form->addElement('date', 'date', array('label' => $st_lang['expenses_date'], 'value' => $st_expense['date']));
+		$form->addElement('submit','submit', array('label' => $st_lang['expenses_send']));
+		$form->addElement('hidden', 'id', array('label' => null, 'value' => $st_expense['id']));
+		return $form;
+	}
+
+	/**
+	 * Returns the monthly expense for a year.
+	 * @return array
+	 */
+	private function getMonthExpensesData() {
+		$st_data = array();
+		$i_dateLimit = date("Y-m-01 00:00:00", strtotime("-12 months"));
+
+		$o_rows = $this->expenses->getMonthExpensesData($_SESSION['user_id'], $i_dateLimit);
+
+		foreach ($o_rows as $key => $value) {
+			try {
+				$timestamp = mktime(0, 0, 0, $value['month'], 1, $value['year']);
+				$st_data[] = array(
+						date("M", $timestamp),
+						(float)$value['amount']
+				);
+			}
+			catch(Exception $e) {
+				error_log(__METHOD__.": ".$e->getMessage());
+				error_log(__METHOD__." data: ".$key." ".print_r($value,true));
+			}
+		}
+		$st_data = array_merge(array(array('Month', 'Expense')), $st_data);
+		return $st_data;
+	}
+
+	/**
+	 * Exports to excel the data currently shown in the view.
+	 * @param array|array[] $st_data containing rows with columns date, amount, name and note.
+	 */
+	private function exportToExcel($st_data) {
+		$this->getResponse()
+				->setHttpResponseCode(200)
+				->setHeader('Content-Type', 'text/csv; charset=utf-8')
+				->setHeader('Content-Disposition', 'attachment; filename='.date('Y-m-d').'.csv')
+				->setHeader('Pragma', 'no-cache')
+				->sendHeaders();
+
+		$output = fopen('php://output', 'w');
+
+		fputcsv($output, array('Fecha', 'Euros', 'Categoria', 'Nota'));
+
+		foreach($st_data as $row) {
+			$outputRow = array(
+					'Fecha' => $row['date'],
+					'Euros' => $row['amount'],
+					'Categoria' => $row['name'],
+					'Nota' => $row['note']
+			);
+			fputcsv($output, $outputRow);
+		}
+		exit(0);
+	}
+
+	/**
+	 * @param array $tags
+	 * @param int $expenseId
+	 * @throws Exception
+	 */
+	private function updateTags($tags, $expenseId) {
+		$existingTags = $this->tags->getTagsByUser($_SESSION['user_id']);
+		foreach($tags as $tag) {
+			$searchTag = str_replace("'", "\\'", $tag);
+			$tagId = array_search($searchTag, $existingTags);
+			if($tagId === FALSE) {
+				$tagId = $this->tags->addTag($_SESSION['user_id'], $tag);
+			}
+			$this->transactionTags->addTagToTransaction($expenseId, $tagId);
+		}
 	}
 
 	/**
