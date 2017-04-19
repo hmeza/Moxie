c9d1f3603439f354464be439d296a828f2d8c4a1
Hugo Meza <hugo.meza.macias@gmail.com>
2017-04-19 17:29:18 +0200

    Created SharedExpenses view and controller

---
+++ application/controllers/SharedExpenses.php
@@ -0,0 +1,11 @@
+<?php
+/** Zend_Controller_Action */
+class SharedExpensesController extends Zend_Controller_Action
+{
+	public function indexAction() {
+	}
+
+	public function createAction() {
+
+	}
+}
