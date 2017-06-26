cfdf98babbb4e1be228ddb55e21a2175387a1b23
Hugo Meza <hugo.meza.macias@gmail.com>
2017-06-26 22:56:55 +0200

    Fix for migration

--- migrations/20170626181953_nullable_shared_expenses_sheet_user.php
+++ migrations/20170626181953_nullable_shared_expenses_sheet_user.php
@@ -4,29 +4,11 @@ use Phinx\Migration\AbstractMigration;
 
 class NullableSharedExpensesSheetUser extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
-    {
-
-    }
+	public function up() {
+		$this->query("alter table shared_expenses_sheet_users modify id_user int(11) null");
+	}
+	
+	public function down() {
+		$this->query("alter table shared_expenses_sheet_users modify id_user int(11) not null");
+	}
 }
