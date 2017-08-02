dbec89d1af7368d7775524987783727fdfd57303
Hugo Meza <hugo.meza.macias@gmail.com>
2017-08-02 14:32:16 +0200

    WIP incomes form with id and delete button

--- application/controllers/ExpensesController.php
+++ application/controllers/ExpensesController.php
@@ -206,7 +206,7 @@ class ExpensesController extends TransactionsController
 		$form->addElement(
 				'date', 'date', array('label' => $st_lang['expenses_date'], 'value' => $st_expense['date'], 'class' => 'form-control')
 				);
-		$form->addElement('submit','submit', array('label' => $st_lang['expenses_header'], 'class' => 'btn btn-info pull-right'));
+		$form->addElement('submit','submit', array('label' => $st_lang['expenses_header'], 'class' => 'btn btn-primary pull-right'));
 		$form->addElement('hidden', 'id', array('label' => null, 'value' => $st_expense['id']));
 		
 		$form->addElement('text', 'tags', array('label' => 'Tags', 'data-role' => 'tagsinput', 'value' => $tag_value));
