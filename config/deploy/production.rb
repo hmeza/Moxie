4ea323bfb32369195e4ba0b207251d7253a0e12f
Hugo Meza <hugo.meza.macias@gmail.com>
2018-09-20 00:29:22 +0200

    Changed ssh port for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,7 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-role :web, %w{ubuntu@moxie.dootic.com}
+role :web, %w{ubuntu@moxie.dootic.com:22022}
 
 
 # Configuration
