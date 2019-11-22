c95ee8604b5e7a4583bd17f00241aa96b81140bb
Hugo Meza <hugo.meza.macias@gmail.com>
2019-11-22 21:50:20 +0100

    Added toggle view columns for expenses column graph.

--- js/expenses/stats.js
+++ js/expenses/stats.js
@@ -31,6 +31,49 @@ var expensesBarRedirector = function(chart, data) {
 }
 
 
+function addTypeDisableListener(columnChart, data2ToDataTable, columnChartData, columnChartOptions) {
+    var columns = [];
+    var series = {};
+    for (var i = 0; i < data2ToDataTable.getNumberOfColumns(); i++) {
+        columns.push(i);
+        if (i > 0) {
+            series[i - 1] = {};
+        }
+    }
+    google.visualization.events.addListener(columnChart, 'select', function () {
+        var sel = columnChart.getSelection();
+        // if selection length is 0, we deselected an element
+        if (sel.length > 0) {
+            // if row is undefined, we clicked on the legend
+            if (sel[0].row === null) {
+                var col = sel[0].column;
+                if (columns[col] == col) {
+                    // hide the data series
+                    columns[col] = {
+                        label: data2ToDataTable.getColumnLabel(col),
+                        type: data2ToDataTable.getColumnType(col),
+                        calc: function () {
+                            return null;
+                        },
+                    };
+
+                    // grey out the legend entry
+                    series[col - 1].color = '#CCCCCC';
+                }
+                else {
+                    // show the data series
+                    columns[col] = col;
+                    series[col - 1].color = null;
+                }
+                data2ToDataTable = new google.visualization.DataView(columnChartData);
+                data2ToDataTable.setColumns(columns);
+                columnChart.draw(data2ToDataTable, columnChartOptions);
+            }
+        }
+    });
+    return data2ToDataTable;
+}
+
 // Callback function
 function drawChart() {
 	// Create the data table.
@@ -64,9 +107,9 @@ function drawChart() {
     chart.draw(data, options);
 
 
-    var data2 = google.visualization.arrayToDataTable(barChartData);
+    var columnData = google.visualization.arrayToDataTable(barChartData);
     width -= 100;
-	var options2 = {
+	var columnGraphOptions = {
 		title: barTitle,
 		hAxis: {title: 'Meses', titleTextStyle: {color: 'red'}},
         vAxis: { viewWindow: { min: 0}},
@@ -75,10 +118,14 @@ function drawChart() {
         legend: {position: 'top'},
         is3D: true
 	};
+	var columnChart = new google.visualization.ColumnChart(document.getElementById('expenses_all'));
+    var view = new google.visualization.DataView(columnData);
+    var data2ToDataTable = view.toDataTable();
 
-	var chart2 = new google.visualization.ColumnChart(document.getElementById('expenses_all'));
-    addDoubleClickListener(chart2, data2, expensesBarRedirector);
-	chart2.draw(data2, options2);
+    columnChart.draw(data2ToDataTable, columnGraphOptions);
+
+    addDoubleClickListener(columnChart, data2ToDataTable, expensesBarRedirector);
+    addTypeDisableListener(columnChart, data2ToDataTable, columnData, columnGraphOptions);
 }
 
 $(window).resize(function() {
