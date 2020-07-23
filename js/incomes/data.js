6e93759915ae8180d222cce43688129072ecad5e
Hugo Meza <hugo.meza.macias@gmail.com>
2020-07-23 20:24:05 +0200

    Removed unused chosen library call raising errors.

--- js/incomes/data.js
+++ js/incomes/data.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
     $('#date_min').attr('type', 'date');
     $('#date_max').attr('type', 'date');
-
-    $("#category_search").chosen({
-        disable_search_threshold: 10,
-        width: "100%"
-    });
 });
